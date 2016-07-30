ac41ebd54936302977b4a30f3a89b8b9ef47060d
Kalle <Kalle@email.me>
2016-07-30 12:17:39 +0200

    Login done

--- app/login.component.ts
+++ app/login.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { DataService} from './data.service';
-import { Http, Response } from '@angular/http';
+import { Http, Response, Headers } from '@angular/http';
 import 'rxjs/add/operator/toPromise';
 
 @Component({
@@ -13,32 +13,58 @@ export class LoginComponent {
     password: string;
     isLoginSuccessful: boolean;
     constructor(private http: Http, private dataService: DataService) {
-        this.http.get("http://localhost:3000/login")
-            .toPromise()
-            .then(response => {
-                console.log('got response:');
-                console.log(response.json());
-                console.log(response.json().data);
-            })
-            .catch();
+        // this.http.get("http://localhost:3000/login")
+        //     .toPromise()
+        //     .then(response => {
+        //         console.log('got response:');
+        //         console.log(response.json());
+        //         console.log(response.json().data);
+        //     })
+        //     .catch();
+
     }
 
     onSubmit() {
-        console.log('tryig to call service...');
-        
-        this.http.get("http://localhost:3000/users")
-            .toPromise()
-            .then(response => {
-                console.log('got response:');
-                console.log(response.json().data);
-            })
-            .catch();
-        this.isLoginSuccessful = this.dataService.isAuthenticatedUser(this.userName, this.password);
-        if (this.isLoginSuccessful) {
-            alert("Login success");
-        }
-        else {
-            alert("Login failed");
-        }
+
+        console.log("trying to login...");
+        console.log("user name: " + this.userName);
+        console.log("password: " + this.password);
+
+        let creds = JSON.stringify({ username: this.userName, password: this.password });
+
+        let headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+
+        this.http.post('http://localhost:3000/login', creds, {
+            headers: headers
+        })
+            .subscribe(
+            data => {
+                console.log("Server response: " + data.json());
+
+                var loginStatus = data.json();
+                if (loginStatus.isAuthenticatedUser) {
+                    alert("Login successful");
+                }
+                else {
+                    alert("Login failed");
+                }
+            });
+        // console.log('tryig to call service...');
+
+        // this.http.get("http://localhost:3000/users")
+        //     .toPromise()
+        //     .then(response => {
+        //         console.log('got response:');
+        //         console.log(response.json().data);
+        //     })
+        //     .catch();
+        // this.isLoginSuccessful = this.dataService.isAuthenticatedUser(this.userName, this.password);
+        // if (this.isLoginSuccessful) {
+        //     alert("Login success");
+        // }
+        // else {
+        //     alert("Login failed");
+        // }
     }
 }
